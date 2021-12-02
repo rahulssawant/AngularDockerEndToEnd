3822efa78f5ad84145342b77323b01fbee27e0da
rahulssawant <rahul.sawant@citiustech.com>
2021-12-02 14:41:11 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularDockerEndToEnd';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(){
+    this.http.get(`http://localhost:8081/WeatherForecast`).subscribe(
+        data => {
+          console.log(data);
+        },
+        err => {
+          console.log(err);
+        }
+      )
+  }
 }
